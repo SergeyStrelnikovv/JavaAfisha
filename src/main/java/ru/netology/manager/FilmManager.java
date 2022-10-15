bdf34ecdcda3685fd82eb88abc0d808570f35b42
sergey <sso96@mail.ru>
2022-10-15 17:56:14 +0300

    100500 repo and 100500commit

---
+++ src/main/java/ru/netology/manager/FilmManager.java
@@ -0,0 +1,35 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Film;
+import ru.netology.repository.FilmRepository;
+
+public class FilmManager {
+    private FilmRepository repository;
+    Film[] films = new Film[0];
+    int filmsQuantity = 10;
+
+    //    public FilmManager() {
+//    }
+    public FilmManager(FilmRepository repository) {
+        this.repository = repository;
+    }
+
+    public void addFilm(Film film) {
+        repository.save(film);
+    }
+
+    public Film[] getAll() {
+        Film[] films = repository.findAll();
+        int resultLength = Math.min(filmsQuantity, films.length);
+        Film[] result = new Film[resultLength];
+        for (int i = 0; i < result.length; i++) {
+            int index = films.length - i - 1;
+            result[i] = films[index];
+        }
+        return result;
+    }
+
+    public Film[] showAll() {
+        return repository.findAll();
+    }
+}
